66b350bd13f74bc8646a0879c54400555ebec9d3
Takesure Mamvota <tooyoungmamvota@gmail.com>
2024-06-24 23:15:39 +0700

    Initial commit

--- app/account/page.jsx
+++ app/account/page.jsx
@@ -1,7 +1,10 @@
-import Navigation from "../components/Navigation";
-
+export const metadata = {
+    title: 'Your Account',
+}
 export default function Page() {
     return <div>
-        <h1>Account Page</h1>
+        <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+            Welcome, Takesure Mamvota
+        </h2>
     </div>
 }
---
+++ app/reservations/page.jsx
@@ -0,0 +1,8 @@
+export const metadata = {
+    title: 'Reservations',
+}
+export default function Page() {
+    return <div>
+        <h1>Reservation Page</h1>
+    </div>
+}
